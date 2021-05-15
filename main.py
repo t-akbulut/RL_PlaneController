7d6524e73575bdad5fb9c108abdf0e657010585d
t-akbulut <tolgahanakbulut@hotmail.com.tr>
2021-05-16 00:21:04 +0300

    dronekit eklendi fbwa modunda roll pitch throttle kontrol fonksiyonlari eklendi

--- main.py
+++ main.py
@@ -1,42 +1,67 @@
 from pymavlink import mavutil
 import time
 from Plane import *
+from dronekit import connect, VehicleMode, LocationGlobal, LocationGlobalRelative
+
 import threading
-plane = Plane()
-the_connection = mavutil.mavlink_connection('udpin:localhost:14550')
+#the_connection = mavutil.mavlink_connection('udpin:localhost:14550')
+connection_string="udpin:localhost:14550";
+vehicle = connect(connection_string, wait_ready=True)
 
 
+def arm_and_takeoff(aTargetAltitude):
+    """
+    Arms vehicle and fly to aTargetAltitude.
+    """
 
+    print("Basic pre-arm checks")
+    # Don't let the user try to arm until autopilot is ready
+    while not vehicle.is_armable:
+        print(" Waiting for vehicle to initialise...")
+        time.sleep(1)
 
+        
+    print("Arming motors")
+    # Copter should arm in GUIDED mode
+    vehicle.mode = VehicleMode("GUIDED")
+    vehicle.commands
+    vehicle.armed = True
 
-def handle_received_packet():
+    while not vehicle.armed:      
+        print(" Waiting for arming...")
+        time.sleep(1)
+
+    print("Taking off!")
+    vehicle.simple_takeoff(aTargetAltitude) # Take off to target altitude
+
+    # Wait until the vehicle reaches a safe height before processing the goto (otherwise the command 
+    #  after Vehicle.simple_takeoff will execute immediately).
     while True:
-        try:
-            istenenpaketler = ["ATTITUDE","GLOBAL_POSITION_INT","VFR_HUD"]
-            pakage = the_connection.recv_match(type=istenenpaketler)
-            if pakage.name == "ATTITUDE":
-                plane.update_attitude(pakage)
-            if pakage.name == "GLOBAL_POSITION_INT":
-                plane.update_positon(pakage)
-            if pakage.name == "VFR_HUD":
-                plane.update_speed(pakage)
-                
-        except:
-            pass
-        time.sleep(0.1)
+        print(" Altitude: ", vehicle.location.global_relative_frame.alt)      
+        if vehicle.location.global_relative_frame.alt>=aTargetAltitude*0.95: #Trigger just below target alt.
+            print("Reached target altitude")
+            break
+        time.sleep(1)
+
+
+def roll_cmd(roll):
+    vehicle.channels.overrides['1'] = 1500 + roll / 45* 500;
+def pitch_cmd(pitch):
+    vehicle.channels.overrides['1'] = 1500 + pitch / 45* 500;
+def throttle_cmd(throttle):
+    vehicle.channels.overrides['1'] = 1000 + throttle / 100* 500;
+    
 
 def handle_logging():
     while True:
-        print(plane.alt)
+        print(vehicle.velocity)
         time.sleep(1)
 
 
 if __name__ == "__main__":
-    the_connection.wait_heartbeat()
-    print("Connection OK")
-    thread_received_packet = threading.Thread(target=handle_received_packet)
-    thread_received_packet.start()
+    #arm_and_takeoff(5)
     print("Packet Listening OK")
     thread_logging = threading.Thread(target=handle_logging)
     thread_logging.start()
+    
     print("Logging OK")
