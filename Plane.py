055854071bc20b205f50bd764af2afc0670329ad
t-akbulut <tolgahanakbulut@hotmail.com.tr>
2021-06-07 00:52:06 +0300

    dqn eklendi. plane classı kullanılır hale getirildi. reward fonk belirlendi

--- Plane.py
+++ Plane.py
@@ -1,50 +1,66 @@
 import math
 import pymap3d as pm
-
+from utils import wrap_180
+from pymavlink import mavutil
+import time
+from dronekit import connect, VehicleMode, LocationGlobal, LocationGlobalRelative
+from utils import *
 class Plane:
-    lat          = 0     # deg
-    lon          = 0     # deg
-    alt          = 0     # msl
-    vx           = 0     # m/s
-    vy           = 0     # m/s
-    vz           = 0     # m/s
-    roll         = 0     # deg
-    pitch        = 0     # deg
-    yaw          = 0     # deg
-    airspeed     = 0     # m/s
-    groundspeed  = 0     # m/s
-    throttle     = 0     # %
 
-    #airfield
-    airfield_lat          = -35.3633143     # deg
-    airfield_lon          = 149.1652330     # deg
-    airfield_alt          = 584.96     # msl
-    airfield_heading      = 354     # deg
+    _connection_string="udpin:localhost:14550";
+    _vehicle = None
 
     def __init__(self):
-        pass
+        self._vehicle = connect(self._connection_string, wait_ready=True)
 
 
-    def update_attitude(self, Packet_Attitude):
-        self.roll = Packet_Attitude.roll   * 180 / math.pi
-        self.pith = Packet_Attitude.pitch  * 180 / math.pi
-        self.yaw = Packet_Attitude.yaw     * 180 / math.pi
+    def get_state(self):
+        pitch = self._vehicle.attitude.pitch * 180 / math.pi
+        vz = self._vehicle.velocity[2] * -1
+        va = self._vehicle.airspeed
+        d = math.sqrt(self._vehicle.location.local_frame.east**2 + self._vehicle.location.local_frame.north**2) 
+        if self._vehicle.location.local_frame.north > 0:
+            d *= -1
+        h = self._vehicle.location.local_frame.down * -1
+        #pitch,as,vz,d,h
+        return [pitch,va,vz,d,h];
 
-    def update_positon(self, Packet_GlobalPosition):
-        self.lat = Packet_GlobalPosition.lat / 10e6
-        self.lon = Packet_GlobalPosition.lon / 10e6
-        self.alt = Packet_GlobalPosition.alt / 1000
-
+    def get_reward(self,state):
+       
+        #pitch reward
+        r_p = abs(state[0]-0) * -1
+        if(r_p>-8):
+            r_p=0
         
-        self.vx = Packet_GlobalPosition.vx / 100
-        self.vy = Packet_GlobalPosition.vy / 100
-        self.vz = Packet_GlobalPosition.vz / 100
-    def update_speed(self, Packet_VfrHud):
-        self.airspeed = Packet_VfrHud.airspeed
-        self.groundspeed = Packet_VfrHud.groundspeed
-        self.throttle = Packet_VfrHud.throtle
+        #airspeed reward
+        r_va = abs(state[1]-22) * -1
 
-    def get_runway_relative_position(self):
-        return pm.geodetic2enu(self.lat, self.lon, self.alt, 
-                               self.airfield_lat, self.airfield_lon, self.airfield_alt)
+        #vz reward
+        r_vz=0
+        if(state[4]< 10):
+            r_vz = abs(state[2]+1)*3 * -1
+        
+        #glide slope reward
+        slope_angle = state[4]/state[3]
+        r_s = abs(slope_angle - 0.05) *5 * -1
+
+        #live reward
+        r_l = 0.5
+
+        return r_p + r_va + r_vz + r_s + r_l
+
+    def act(self,action):
+        state = self.get_state()
+        # action [throttle(0,100),roll(-100,100)]
+        self._vehicle.channels.overrides['1'] = int(1500 + action[1] / 100 * 500)
+        self._vehicle.channels.overrides['3'] = int(1000 + action[0] / 100 * 1000)
+        time.sleep(0.1)
+        new_state = self.get_state()
+        reward = self.get_reward(new_state)
+        return [state,action,new_state,reward]
+        
+
+
+
+    
     
--- main.py
+++ main.py
@@ -1,81 +1,28 @@
+from operator import ne
 from utils import wrap_180
-from pymavlink import mavutil
 import time
 from Plane import *
-from dronekit import connect, VehicleMode, LocationGlobal, LocationGlobalRelative
 from utils import *
+from Dqn import *
 
 import threading
-#the_connection = mavutil.mavlink_connection('udpin:localhost:14550')
-connection_string="udpin:localhost:14550";
-vehicle = connect(connection_string, wait_ready=True)
+batch_size = 32
 
-
-def arm_and_takeoff(aTargetAltitude):
-    """
-    Arms vehicle and fly to aTargetAltitude.
-    """
-
-    print("Basic pre-arm checks")
-    # Don't let the user try to arm until autopilot is ready
-    while not vehicle.is_armable:
-        print(" Waiting for vehicle to initialise...")
-        time.sleep(1)
-
-        
-    print("Arming motors")
-    # Copter should arm in GUIDED mode
-    vehicle.mode = VehicleMode("GUIDED")
-    vehicle.commands
-    vehicle.armed = True
-
-    while not vehicle.armed:      
-        print(" Waiting for arming...")
-        time.sleep(1)
-
-    print("Taking off!")
-    vehicle.simple_takeoff(aTargetAltitude) # Take off to target altitude
-
-    # Wait until the vehicle reaches a safe height before processing the goto (otherwise the command 
-    #  after Vehicle.simple_takeoff will execute immediately).
-    while True:
-        print(" Altitude: ", vehicle.location.global_relative_frame.alt)      
-        if vehicle.location.global_relative_frame.alt>=aTargetAltitude*0.95: #Trigger just below target alt.
-            print("Reached target altitude")
-            break
-        time.sleep(1)
-
-
-def roll_cmd(roll):
-    vehicle.channels.overrides['1'] = int(1500 + roll / 50 * 500)
-def pitch_cmd(pitch):
-    vehicle.channels.overrides['1'] = 1500 + pitch / 50* 500
-def throttle_cmd(throttle):
-    vehicle.channels.overrides['1'] = 1000 + throttle / 100* 500
-
-def  navigation():
-    vh= vehicle.heading
-    ph = math.atan2(vehicle.location.local_frame.east*-1,vehicle.location.local_frame.north*-1) * 180 / math.pi
-    heading_error = wrap_180(vh - ph)
-   
-    print(heading_error)
-    roll_command = constrain(heading_error,-10,10)
-
-    roll_cmd(roll_command*-1)
-    
-
-def handle_logging():
-    yon = 1
-    while True:
-        
-        #roll_cmd(yon * 10)
-        navigation()
-        time.sleep(1)
+plane = None
 
 if __name__ == "__main__":
-    #arm_and_takeoff(5)
-    print("Packet Listening OK")
-    thread_logging = threading.Thread(target=handle_logging)
-    thread_logging.start()
-    
+    plane = Plane()
+    state_size = 5
+    action_size = 2
+    agent = Dqn(state_size, action_size)
+    #thread_logging = threading.Thread(target=handle_logging)
+    #thread_logging.start()
+    state = plane.get_state()
+    while True:
+        action = agent.act(state)
+        [state,action,new_state,reward] = plane.act(action)
+        agent.memorize(state,action,reward,new_state,False)
+        print(state,plane.get_reward(state))
+        if len(agent.memory) > batch_size:
+                agent.replay(batch_size)
     print("Logging OK")
